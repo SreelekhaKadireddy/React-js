85861109b40544a9232b68f0402e22979c0412cd
SreelekhaKadireddy <ksreelekha049@gmail.com>
2023-11-30 14:29:30 +0530

    react

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Useeffect_ex from "./hooks/useeffect";
 import Usestate_ex from "./hooks/usestate";
+import ReistrationForm from "./hooks/useref";
+import Factorial from "./hooks/usememo";
 export default function App(){
   return(
     <>
     <Usestate_ex/>
     <hr/>
     <Useeffect_ex/>
+    <hr/>
+    <ReistrationForm />
+    <hr/>
+    <Factorial/>
     </>
   )
 }
